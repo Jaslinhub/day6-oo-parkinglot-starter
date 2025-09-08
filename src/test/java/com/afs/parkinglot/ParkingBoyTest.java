66a43b08b03998e40e54aedf9e3da01d931ac308
Jasmine <jasmine.lin@oocl.com>
2025-09-08 20:55:55 +0800

    feat:parking_boy park car in the parking lot with error message

--- src/test/java/com/afs/parkinglot/ParkingBoyTest.java
+++ src/test/java/com/afs/parkinglot/ParkingBoyTest.java
@@ -2,8 +2,7 @@ package com.afs.parkinglot;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ParkingBoyTest {
     @Test
@@ -21,5 +20,57 @@ public class ParkingBoyTest {
         Car fetchedCar=parkingBoy.fetch(parkingBoy.park(car));
         assertNotNull(fetchedCar);
         assertSame(car, fetchedCar);
+    }
+    //Given two parked cars, a standard parking boy, and two tickets When fetch the car twice, Then return the right car with each ticket
+    @Test
+    void should_return_each_parked_car_when_given_two_tickets(){
+        Car car1=new Car();
+        Car car2=new Car();
+        ParkingLot parkingLot=new ParkingLot(100);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        Ticket ticket1=parkingBoy.park(car1);
+        Ticket ticket2=parkingBoy.park(car2);
+        Car fetchedCar1=parkingBoy.fetch(ticket1);
+        Car fetchedCar2=parkingBoy.fetch(ticket2);
+        assertNotNull(fetchedCar1);
+        assertNotNull(fetchedCar2);
+        assertSame(car1, fetchedCar1);
+        assertSame(car2, fetchedCar2);
+    }
+    //Given a parking lot with a parked car, a wrong ticket to fetch the car then return error message
+    @Test
+    void should_return_nothing_when_wrong_ticket_given(){
+        Car car=new Car();
+        ParkingLot parkingLot=new ParkingLot(100);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        parkingBoy.park(car);
+        Ticket wrongTicket=new Ticket();
+        assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(wrongTicket);
+        });
+    }
+    //Given a parking lot with a parked car, a used ticket to fetch the car then return error message
+    @Test
+    void should_return_nothing_when_used_ticket_given(){
+        Car car=new Car();
+        ParkingLot parkingLot=new ParkingLot(100);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        Ticket usedTicket=parkingBoy.park(car);
+        parkingBoy.fetch(usedTicket);
+        assertThrows(UnrecognizedTicketException.class, () -> {
+            parkingBoy.fetch(usedTicket);
+        });
+    }
+    //a parking lot without capacity, a car to park then return error message
+    @Test
+    void should_return_nothing_when_parking_lot_is_full(){
+        Car car1=new Car();
+        Car car2=new Car();
+        ParkingLot parkingLot=new ParkingLot(1);
+        ParkingBoy parkingBoy=new ParkingBoy(parkingLot);
+        parkingBoy.park(car1);
+        assertThrows(IllegalStateException.class, () -> {
+            parkingBoy.park(car2);
+        });
     }
 }
